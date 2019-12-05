aa883d99d23336328457cdf7b20f37a07a751e2c
Lawrence <56071035+LLLefatle1124@users.noreply.github.com>
2019-12-06 00:37:07 +0200

    Night Work. pictures displaying

--- memoryGame.js
+++ memoryGame.js
@@ -1,55 +1,56 @@
+// storing game Objects in an Array
+var images = new Array(13)
+
+for(var i = 1; i <= 13; i++){
+  images[i] = new Image()
+  images[i].src = "Media/" + i + ".png"
+}
+
+// Create a 2-dimensional array 
+var gfg = new Array(4); 
+
+// Creating gameSpace
 function createGameSpace(){
-  // storing game Objects in an Array
-  var images = new Array(13)
-  
-  // // var pics = ["Media/1.png", "Media/2.png", "Media/3.png", "Media/4.png", "Media/5.png", "Media/6.png", "Media/7.png", "Media/8.png", "Media/9.png", "Media/10.png", "Media/11.png", "Media/12.png"]
-
-  for(var i = 1; i <= 13; i++){
-    images[i] = new Image()
-    images[i].src = "Media/" + i + ".png"
-  }
-
-  // Create one dimensional array 
-  var gfg = new Array(4); 
-
-  // Loop to create 2D array using 1D array 
+  // Loop to create and display the initial game space. 
   for (var i = 0; i < gfg.length; i++) { 
     gfg[i] = new Array(3); 
   }
 
-  pictureNumber = 0
-  
-  // Loop to initilize 2D array elements. 
   for (var i = 0; i < 3; i++) { 
     for (var j = 0; j < 4; j++){ 
-      
-      gfg[i][j] = images[++pictureNumber];
+      gfg[i][j] = '<img src="Media/13.png"></img>'
+      document.write(gfg[i][j])
     }
-  }
-
-  // Loop to display the elements of 2D array. 
-  for (var i = 0; i < 3; i++) { 
-
-    for (var j = 0; j < 4; j++) { 
-    document.write('<img src= "images[i].src"> </img>' + " "); 
-    }
-
     document.write("<br>")
-
-  } 
-
-
-
-  var myImages = ""
-  for(var i = 1; i<5; i++){
-    // for(var j = o; j<; j++){
-      myImages += '<img src= "images[i].src"> </img>'
-    // }
   }
 
-  document.write(myImages)
-  
   return gfg;
 }
 
+// plotting in gamepace
+function plotGameComponets(){
+  for (var i = 0; i < 3; i++) {
+    for (var j = 0; j < 4; j++) {
+      gfg[i][j] = '<img src="Media/13.png"></img>'
+      document.write(gfg[i][j])
+    }
+    document.write("<br>")
+  }
+}
+
+// displaying gameSpace
+function displayInGameSpace(arr){
+  // Loop to display the elements of 2D array. 
+  for (var i = 0; i < 3; i++) { 
+    for (var j = 0; j < 4; j++) { 
+      if (arr[i][j] !== '<img src="Media/13.png"></img>'){
+        document.write(arr[i][j])
+      }
+    }
+    document.write("<br>")
+  } 
+}
+
+// my function calls
 createGameSpace();
+// displayInGameSpace()
