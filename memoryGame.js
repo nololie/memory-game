7bd90c35d59e56c9ef63bba5ce8d422acca2cdc1
Lawrence <56071035+LLLefatle1124@users.noreply.github.com>
2019-12-13 07:27:40 +0200

    Now fixed

--- memoryGame.js
+++ memoryGame.js
@@ -3,40 +3,44 @@ var images = new Array(13)
 
 // storing game Objects in an Array
 function storeGameObjects(){
+  
   images[0] = '<img id="coverimage" src="Media/13.png"></img>'
   for(var i = 1; i < 13; i++){
     images[i] = '<img id="imge' + i + '" src="Media/'+ i +'.png"></img>'
   }
+
 }
 
 // plotting in gamepace
-function plotGameComponets(gfg){
+function plotGameComponets(array){
+  
   storeGameObjects()
   imagIindex = 0
+
   for (var i = 0; i < 3; i++) {
     for (var j = 0; j < 4; j++) {
       ++imagIindex
-      gfg[i][j] = images[imagIindex]
+      array[i][j] = images[imagIindex]
     }
+  } 
 
-  }    
 }
 
 // Create a 2-dimensional array 
 var myGameSpace = new Array(3); 
 
 // Creating gameSpace
-function createGameSpace(gfg){
+function createGameSpace(array){
   world = '<div id="#myGameSpaceWorld">'
   
   // Loop to create and display the initial game space.
-  for (var i = 0; i < gfg.length; i++) { 
+  for (var i = 0; i < array.length; i++) { 
     
-    gfg[i] = new Array(4)
+    array[i] = new Array(4)
 
     for (var j = 0; j < 4; j++) { 
-      gfg[i][j] = '<img id="coverimage" src="Media/13.png"></img>'
-      world += gfg[i][j]
+      array[i][j] = '<img id="coverimage" src="Media/13.png"></img>'
+      world += array[i][j]
     }
 
     world += '<br>'
@@ -50,5 +54,6 @@ function createGameSpace(gfg){
 
 // my function calls
 document.write(createGameSpace(myGameSpace))
+
 // plotGameComponets(myGameSpace)
-// displayInGameSpace(gfg)
+// displayInGameSpace(array)
