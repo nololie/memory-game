0ccc4c3620bed2f69314d2bd123c667408e7c7fa
Lawrence <56071035+LLLefatle1124@users.noreply.github.com>
2019-12-24 00:16:16 +0200

    All fliping. Just the flipback & matching missing

--- memoryGame.js
+++ memoryGame.js
@@ -1,59 +1,111 @@
+
 // creating an array to store htmlelements
-var images = new Array(13)
+var images = new Array(12)
 
 // storing game Objects in an Array
-function storeGameObjects(){
-
-  images[0] = '<img id="coverimage" src="Media/13.png"></img>'
-  for(var i = 1; i < 13; i++){
-    images[i] = '<img id="imge' + i + '" src="Media/'+ i +'.png"></img>'
+function storeGameObjects(array){
+  console.log('before shuffle==================================')
+  for(var i = 1; i <= 12; i++){
+    var gameElements = document.createElement('img')
+    gameElements.setAttribute('id', "imge" + i)
+    gameElements.setAttribute('src', "Media/" + i + ".png")
+    array[i] = gameElements
+    console.log(array[i])
   }
-
 }
 
-// plotting in gamepace
-function plotGameComponets(array){
-  
-  storeGameObjects()
-  imagIindex = 0
 
-  for (var i = 0; i < 3; i++) {
-    for (var j = 0; j < 4; j++) {
-      ++imagIindex
-      array[i][j] = images[imagIindex]
-    }
-  } 
-
+// shuffling gamepace
+function shaffle(array){
+  storeGameObjects(array)
+  console.log('after shuffle====================================')
+  array.sort(function(a, b){return 0.5 - Math.random()})
+  for (var i = 1; i <= 12; i++) {
+  console.log(array[i])
+  }
 }
 
 // Create a 2-dimensional array 
-var myGameSpace = new Array(3); 
+var myGameSpace = new Array(3)
 
 // Creating gameSpace
 function createGameSpace(array){
-  world = '<div id="#myGameSpaceWorld">'
+  
+    world = ''
   
   // Loop to create and display the initial game space.
-  for (var i = 0; i < array.length; i++) { 
+  for (var i = 0; i < array.length; i++){ 
     
     array[i] = new Array(4)
-
+    
     for (var j = 0; j < 4; j++) { 
-      array[i][j] = '<img id="coverimage" src="Media/13.png"></img>'
+      array[i][j] = '<img id="coverimage ' + i + "-" + j + '" src="Media/13.png" onClick=' + "flipImage(" + i + "," + j + ")" +'></img>'  //Beautiful-PERFECT
       world += array[i][j]
     }
-
+    
     world += '<br>'
   
   }
+
   world += "</div>"
-  
+
+  world += '<button id="startGame">Restart</button>'
+
   return world;
 
 }
 
-// my function calls
-document.write(createGameSpace(myGameSpace))
+function restart(){
+  element.innerHTML = createGameSpace(myGameSpace);
+}
 
-// plotGameComponets(myGameSpace)
-// displayInGameSpace(array)
+function flipImage(row, col){
+      if(row == 1){
+        element.replaceChild(images[col], document.getElementById('coverimage ' + row + '-' + col))
+      }else if(row == 2){
+        element.replaceChild(images[col+4], document.getElementById('coverimage ' + row + '-' + col))
+      }else{
+        element.replaceChild(images[col + 8], document.getElementById('coverimage ' + row + '-' + col))
+      }
+}
+
+var element = document.createElement('div');
+element.setAttribute('id', "TheeGameSpace")
+element.innerHTML = createGameSpace(myGameSpace);
+document.body.appendChild(element);
+
+var play = document.getElementById('startGame')
+
+play.addEventListener('onClick', shaffle(images))
+
+// function playGame(array){
+//   rearange(myGameSpace)
+//   startTimer()
+  
+//   // wait for a click and update the gameworld
+
+//   flip1Index = 0
+//   flip2Index = 0
+
+//   var flipCount = 0
+//   if (flipCount <= 2){
+//     flipImage()
+//     flipCount ++
+//     if (flipCount == 2 && !match){
+//       flipCount = 0
+//       // Unflip
+//     }
+//   }
+// }
+
+// // storing background/hidden gamecomponets
+// storeGameObjects()
+
+// // initialising myGameWorld
+// // window.document.body.innerHTML = createGameSpace(myGameSpace) ===========    
+// myGameWorld = createGameSpace(myGameSpace)
+
+// // printing out myGameWorld
+// document.write(myGameWorld)
+
+// // now we start playing
